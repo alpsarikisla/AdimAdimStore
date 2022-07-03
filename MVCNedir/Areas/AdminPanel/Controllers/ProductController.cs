68225024237a665ca9dffb4ee63733d4e4407117
Alp Sarıkışla <50963484+alpsarikisla@users.noreply.github.com>
2022-07-03 12:08:41 +0300

    Ürün Ekleme İşlemi Tamamlandı

---
+++ MVCNedir/Areas/AdminPanel/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using MVCNedir.Filters;
+using MVCNedir.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCNedir.Areas.AdminPanel.Controllers
+{
+    [AdminPanelAuthenticationFilter]
+    public class ProductController : Controller
+    {
+        AdimAdimDBModel db = new AdimAdimDBModel();
+        // GET: AdminPanel/Product
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.Category_ID = new SelectList(db.Categories, "ID", "Isim");
+            ViewBag.Brand_ID = new SelectList(db.Brands, "ID", "Isim");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product model, HttpPostedFileBase resim)
+        {
+            if (ModelState.IsValid)
+            {
+                if (resim != null)
+                {
+                    FileInfo fi = new FileInfo(resim.FileName);
+                    if (fi.Extension == ".jpg" || fi.Extension == ".png" || fi.Extension == ".jpeg")
+                    {
+                        string isim = Guid.NewGuid().ToString() + fi.Extension;
+                        model.ImagePath = isim;
+                        resim.SaveAs(Server.MapPath("~/ProductImages/" + isim));
+                    }
+                    else
+                    {
+                        model.ImagePath = "noneproduct.jpg";
+                    }
+                }
+                else
+                {
+                    model.ImagePath = "noneproduct.jpg";
+                }
+                model.CreationTime = DateTime.Now;
+                db.Products.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Category_ID = new SelectList(db.Categories, "ID", "Isim");
+            ViewBag.Brand_ID = new SelectList(db.Brands, "ID", "Isim");
+            return View(model);
+        }
+    }
+}
--- MVCNedir/Models/Product.cs
+++ MVCNedir/Models/Product.cs
@@ -21,19 +21,30 @@ namespace MVCNedir.Models
         [ForeignKey("Brand_ID")]
         public virtual Brand Brand { get; set; }
 
+        [Display(Name = "Ürün Adı")]
         [Required(ErrorMessage = "Bu alan zorunludur")]
         [StringLength(maximumLength:75, ErrorMessage ="bu alan en fazla 75 karkater olmalıdır")]
         public string Name { get; set; }
 
+        [Display(Name = "Açıklama")]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
+        [Display(Name = "Ürün Resim")]
         [StringLength(maximumLength: 150)]
         public string ImagePath { get; set; }
 
+        [Display(Name = "Stok Adet")]
         public int Stock { get; set; }
 
+        [DataType(DataType.Currency)]
+        [Display(Name = "Fiyat")]
         public double Price { get; set; }
 
+        [Display(Name = "Ekleme Tarihi")]
+        public DateTime CreationTime { get; set; }
+
+        [Display(Name = "Satış Durum")]
         public bool SellStatus { get; set; }
     }
 }
